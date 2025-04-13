a558afa5c6717890815535b7f3418dc72464c0f4
idkdevgenerate <BeatCanvasMusicYT@gmail.com>
2025-04-13 22:16:01 +0100

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_fCEsZfxYbvxNTg0M2V55Db9m"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
